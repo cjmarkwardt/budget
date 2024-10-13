8214fb4a7520b418c971e4bbd83d3d4dfb630208
Chris Markwardt <cjmarkwardt95@gmail.com>
2024-10-12 19:59:57 -0500

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    $('#test').text("Tester2");
+    gapi.load('client', function() {
+        gapi.client.init({
+            'apiKey': 'AIzaSyA6GNo9Kbz8ojuAq8g4pYQcNYzVVp2yCNU'
+        }).then(function() {
+        // 3. Initialize and make the API request.
+            return gapi.client.request({
+              'path': 'https://sheets.googleapis.com/v4/spreadsheets/1J7GDw2LZI2VVYNdQS5qCWouRF2krGv53ihJ6iXZYMWY/values/Funds!A3:D',
+            })
+        }).then(function(response) {
+            console.log(response.result);
+            $('#test').text("Tester2");
+        }, function(reason) {
+            console.log('Error: ' + reason.result.error.message);
+        });
+    });
 });
