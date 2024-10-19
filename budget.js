c1b0a0baa31bb36c3dfc5bebb9c5710859b19be6
Chris Markwardt <cjmarkwardt95@gmail.com>
2024-10-19 17:01:19 -0500

    Update budget.js

--- budget.js
+++ budget.js
@@ -4,24 +4,20 @@ $(document).ready(() => {
             'apiKey': 'AIzaSyA6GNo9Kbz8ojuAq8g4pYQcNYzVVp2yCNU'
         }).then(() => {
             return gapi.client.request({
-              'path': 'https://sheets.googleapis.com/v4/spreadsheets/1J7GDw2LZI2VVYNdQS5qCWouRF2krGv53ihJ6iXZYMWY/values/Funds!A3:D',
+              'path': 'https://sheets.googleapis.com/v4/spreadsheets/1vKHZzgwLFO9m05j1zFCVbljER3jrc-7H_bkz6mP300c/values/Funds!A3:D',
             })
         }).then((response) => {
             console.log(response.result);
-            let stop = false;
             response.result.values.forEach((row) => {
-                if (!stop) {
-                    if (row.length == 0) {
-                        stop = true;
-                    }
-                    
+                if (row[0] == 'X')
+                {
                     let style = '';
                     
                     if (row[3].includes('-')) {
                         style += 'color:red';
                     }
                     
-                    $('#table').append('<tr style=\'' + style + '\'><td>' + row[0] + '</td><td>' + row[3] + '</td></tr>');
+                    $('#table').append('<tr style=\'' + style + '\'><td>' + row[1] + '</td><td>' + row[6] + '</td></tr>');
                 }
             });
             $('#test').text("Tester3");
